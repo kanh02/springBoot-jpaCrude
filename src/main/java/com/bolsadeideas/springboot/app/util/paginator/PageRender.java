a855365d41eef6128dd1740ac48214abad6e5a13
unknown <oscari.rueda@oirueda650w10.babel-si.local>
2019-11-08 14:47:04 +0100

    ejercicio crude springboot con jpa

---
+++ src/main/java/com/bolsadeideas/springboot/app/util/paginator/PageRender.java
@@ -0,0 +1,83 @@
+package com.bolsadeideas.springboot.app.util.paginator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+public class PageRender <T>{
+	
+	private String url;
+	private Page<T> page;
+	
+	private int totalPaginas;
+	
+	private int numElementosPorpagina;
+	
+	private int paginaActual;
+	
+	private List<PageItem> paginas;
+	
+	public PageRender(String url, Page<T> page) {
+		this.url = url;
+		this.page = page;
+		this.paginas = new ArrayList<PageItem>();
+		
+		numElementosPorpagina = page.getSize();
+		totalPaginas = page.getTotalPages();
+		paginaActual = page.getNumber() +1;
+		
+		int desde, hasta;
+		if(totalPaginas <= numElementosPorpagina) {
+			desde = 1;
+			hasta = totalPaginas;
+		}else {
+			if(paginaActual <= numElementosPorpagina/2) {
+				desde = 1;
+				hasta = numElementosPorpagina;
+			}else if(paginaActual >= totalPaginas - numElementosPorpagina/2){
+				 desde = totalPaginas - numElementosPorpagina +1;
+				 hasta = numElementosPorpagina;
+			}else {
+				desde = paginaActual - numElementosPorpagina/2;
+				hasta = numElementosPorpagina;
+			}
+		}
+		
+		for(int i=0; i < hasta; i++) {
+			paginas.add(new PageItem(desde + i, paginaActual == desde + i));
+		}
+		 
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public int getTotalPaginas() {
+		return totalPaginas;
+	}
+
+	public int getPaginaActual() {
+		return paginaActual;
+	}
+
+	public List<PageItem> getPaginas() {
+		return paginas;
+	}
+	
+	public boolean isFirst() {
+		return page.isFirst();
+	}
+	public boolean isLast() {
+		return page.isLast();
+	}
+	public boolean isHasNext() {
+		return page.hasNext();
+	}
+	public boolean isHasPrevious() {
+		return page.hasPrevious();
+	}
+	
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,24 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(3, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(4, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(5, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(6, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(7, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(8, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(9, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(10, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(11, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(12, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(13, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(14, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(15, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(16, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(17, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(18, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(19, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(20, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(21, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(22, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+
